124b97b88e9bb146116508e0467d23fdff9cd323
Daniel Shlomo <daniel.s@rvconnex.com>
2020-07-21 17:10:31 +0700

    fixed

--- prog.js
+++ prog.js
@@ -51,24 +51,32 @@ customElements.define('p-script', script);
 customElements.define('p-img', img);
 customElements.define('p-content', content);
 
-
-function prog(anchor) {
-    this.cloneAttributes = (source, target) => {
+function prog() {
+    const anchor = '.prog'
+    this.cloneAttributes = (source, target, exclude) => {
+        if(!exclude){
+            exclude = [];
+        }
         for (let i = 0; i < source.attributes.length; i++) {
-            target.setAttribute(source.attributes[i].name, source.attributes[i].value)
+            if (!exclude.includes(source.attributes[i].name)) {
+                target.setAttribute(source.attributes[i].name, source.attributes[i].value)
+            }
         }
         return target;
+    }
+
+    this.fetch_retry = (url, options, n) => {
+        return fetch(url, options).catch(function (error) {
+            if (n === 1) throw error;
+            return fetch_retry(url, options, n - 1);
+        });
     }
 
     const elems = document.querySelectorAll(anchor);
     const header = document.getElementsByTagName("head")[0];
     const body = document.getElementsByTagName("body")[0];
-    const prog_style = document.createElement('style');
-    prog_style.setAttribute('id', 'prog_style');
-    header.appendChild(prog_style);
-    const generalStyle = document.getElementById('prog_style');
 
-    observer = new IntersectionObserver(entries => {
+    this.observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             const t = entry.target,
                 pIn = t.getAttribute('p-in'),
@@ -78,13 +86,41 @@ function prog(anchor) {
                 if (!t.getAttribute('p-mem')) {
                     const tag = t.tagName
                     let e;
+                    let placeholder;
                     switch (tag) {
                         case 'P-IMG':
+                            placeholder = t.getAttribute('placeholder');
+                            const placeholderStyle = t.getAttribute('placeholder-style');
+                            const url = t.innerHTML;
                             e = document.createElement('img');
-                            e = this.cloneAttributes(t, e)
-                            e.setAttribute('src',t.innerHTML.trim())
+                            e = this.cloneAttributes(t, e, ['placeholder','placeholder-style']);
+                            if(placeholder){
+                                e.setAttribute('src', placeholder)
+                            }
+                            if(placeholderStyle){
+                                e.setAttribute('style', t.getAttribute('style')+placeholderStyle)
+                            }
                             t.innerHTML = ''
                             t.appendChild(e)
+
+                            var final = new Image();
+                            final = this.cloneAttributes(t, final, ['placeholder','placeholder-style']);
+                            final.onload = function () {
+                                t.removeChild(e);
+                                t.appendChild(final)
+                            }
+                            final.src = url;
+
+                            /*e = document.createElement('img');
+                            e = this.cloneAttributes(t, e);
+                            if (placeholder) {
+                                e.setAttribute('src', placeholder)
+                            }
+                            t.innerHTML = ''
+                            t.appendChild(e)
+
+                            e.setAttribute('src', t.innerHTML.trim())*/
+
                             break;
                         case 'P-LINK':
                             e = document.createElement('link');
@@ -99,10 +135,15 @@ function prog(anchor) {
                             break;
                         case 'P-CONTENT':
                             const src = t.innerHTML.trim()
+                            placeholder = t.getAttribute('placeholder')
+                            const retry = t.getAttribute('retry')
+                            t.innerHTML = placeholder;
                             if (src) {
-                                fetch(src)
+                                fetch_retry(src, {}, Number(retry))
                                     .then(response => response.text())
-                                    .then(data => t.innerHTML = data);
+                                    .then(data => t.innerHTML = data).catch((e) => {
+                                        console.log(e)
+                                    })
                             }
                             break;
                     }
@@ -114,7 +155,7 @@ function prog(anchor) {
                 }
 
                 if (!pActive) {
-                    observer.unobserve(t);
+                    this.observer.unobserve(t);
                 }
             } else {
                 if (pOut) {
@@ -125,9 +166,8 @@ function prog(anchor) {
     });
 
     elems.forEach(el => {
-        observer.observe(el);
+        this.observer.observe(el);
     });
+
+    return this;
 }
-document.addEventListener('DOMContentLoaded', function () {
-    prog('.prog')
-});
